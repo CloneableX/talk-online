8b16b0fb5bb2794115c3a9531329aa9b073376b6
Cloneable <cloneable@163.com>
2022-12-13 11:02:14 +0800

    delete message controller and room channel

--- app/channels/rooms_channel.rb
+++
@@ -1,9 +0,0 @@
-class RoomsChannel < ApplicationCable::Channel
-  def subscribed
-    stream_from 'rooms'
-  end
-
-  def self.send_message(message)
-    ActionCable.server.broadcast('rooms', message: message)
-  end
-end
