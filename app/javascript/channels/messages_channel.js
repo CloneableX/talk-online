f1c616aafe9ede003fbb2eae57cb4488647061b4
Cloneable <cloneable@163.com>
2022-12-14 14:38:55 +0800

    show received message

--- app/javascript/channels/messages_channel.js
+++ app/javascript/channels/messages_channel.js
@@ -1,13 +1,25 @@
 import consumer from "./consumer"
 
 consumer.subscriptions.create("MessagesChannel", {
+  initialized() {
+    this.sendMessage = this.sendMessage.bind(this)
+  },
+
   connected() {
     this.install()
   },
 
+  received(data) {
+    $('.message-container').append(`<li>${data.message_text}</li>`)
+  },
+
+  sendMessage() {
+    const messageTextArea = $('textarea');
+    this.send({message_text: messageTextArea.val()})
+    messageTextArea.val('')
+  },
+
   install() {
-    $('#send-button').on('click', () => {
-      this.send({ message_text: $('textarea').val() })
-    })
+    $('#send-button').on('click', this.sendMessage)
   }
 });
