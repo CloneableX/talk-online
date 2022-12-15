28ecfeda7dfee9e2a2f5f4a6ad133d86a51b0402
Cloneable <cloneable@163.com>
2022-12-15 15:41:31 +0800

    load js file by controller name

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,8 @@ module ApplicationHelper
     file_path = "#{Webpacker.config.source_entry_path}/#{filename}.js"
     javascript_pack_tag(filename, *options) if File.exists?(file_path)
   end
+
+  def page_class
+    format('%s-%s-page', controller_name, action_name)
+  end
 end
--- app/javascript/src/rooms/show.js
+++ app/javascript/src/rooms/show.js
@@ -1,16 +1,19 @@
 import messagesChannel from "../../channels/messages_channel";
+import {pageFilter} from "../../components/filters";
 
 $(document).on('turbolinks:load', () => {
-  const sendMessage = () => {
-    const messageTextArea = $('textarea');
-    channel.sendMessage(messageTextArea.val())
-    messageTextArea.val('')
-  }
+  pageFilter('rooms-show-page', () => {
+    const sendMessage = () => {
+      const messageTextArea = $('textarea');
+      channel.sendMessage(messageTextArea.val())
+      messageTextArea.val('')
+    }
 
-  const displayMessage = (messageHtml) => {
-    $('.message-container').append(messageHtml)
-  }
+    const displayMessage = (messageHtml) => {
+      $('.message-container').append(messageHtml)
+    }
 
-  const channel = messagesChannel.create(displayMessage)
-  $('#send-button').on('click', sendMessage)
+    const channel = messagesChannel.create(displayMessage)
+    $('#send-button').on('click', sendMessage)
+  })
 })
