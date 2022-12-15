ae08c42ba32aab1c8a35f42ddb869b9410085651
Cloneable <cloneable@163.com>
2022-12-15 13:52:53 +0800

    refactor: move logical code into special js file

--- app/javascript/channels/messages_channel.js
+++ app/javascript/channels/messages_channel.js
@@ -1,25 +1,21 @@
 import consumer from "./consumer"
 
-consumer.subscriptions.create("MessagesChannel", {
-  initialized() {
-    this.sendMessage = this.sendMessage.bind(this)
-  },
+const messagesChannel = {
+  create: (receivedFun) => {
+    return consumer.subscriptions.create("MessagesChannel", {
+      initialized() {
+        this.sendMessage = this.sendMessage.bind(this)
+      },
 
-  connected() {
-    this.install()
-  },
+      received(data) {
+        receivedFun.call(this, data.html)
+      },
 
-  received(data) {
-    $('.message-container').append(data.html)
-  },
-
-  sendMessage() {
-    const messageTextArea = $('textarea');
-    this.send({message_text: messageTextArea.val()})
-    messageTextArea.val('')
-  },
-
-  install() {
-    $('#send-button').on('click', this.sendMessage)
+      sendMessage(message) {
+        this.send({message_text: message})
+      },
+    });
   }
-});
+}
+
+export default messagesChannel
--- app/javascript/src/rooms/show.js
+++ app/javascript/src/rooms/show.js
@@ -1,3 +1,16 @@
+import messagesChannel from "../../channels/messages_channel";
+
 $(document).on('turbolinks:load', () => {
-  console.log('Room load!!')
+  const sendMessage = () => {
+    const messageTextArea = $('textarea');
+    channel.sendMessage(messageTextArea.val())
+    messageTextArea.val('')
+  }
+
+  const displayMessage = (messageHtml) => {
+    $('.message-container').append(messageHtml)
+  }
+
+  const channel = messagesChannel.create(displayMessage)
+  $('#send-button').on('click', sendMessage)
 })
