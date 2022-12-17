d7c8fa1f1666ee182a13bd0067725eeee53c78d3
Cloneable <cloneable@163.com>
2022-12-17 15:10:53 +0800

    tasking

--- spec/models/room_spec.rb
+++ spec/models/room_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Room, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+#   TODO: create room
+#     TODO: create room with 4 random number
+#       TODO: number is 4 random uppercase letters
+#       TODO: number must be unique
+#     TODO: join room by input room number
+#       TODO: redirect back to root page when room number is incorrect
+#       TODO: join room when room number is correct
 end
